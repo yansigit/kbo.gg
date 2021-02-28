38343e34690479c5c1c4da448f0549b204770b3e
gem <44089734+yansigit@users.noreply.github.com>
2021-02-28 17:23:15 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {createRef, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Line} from 'react-chartjs-2';
 
 function App() {
+  useEffect(() => {})
+
+  const data = {
+    labels: ['선수1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [50, 65, 25, 30, 43, 80, 90, 95, 100],
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
+  }
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Line data={data} options={options} />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload!!
         </p>
         <a
           className="App-link"
